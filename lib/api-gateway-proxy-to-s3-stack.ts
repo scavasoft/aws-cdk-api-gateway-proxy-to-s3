140ab8d4ebe93c327f81717477dea510e16b9367
Dzhuneyt Ahmed <dzhuneyt@dzhuneyt.com>
2021-01-18 11:57:10 +0200

    feat: Experiment on how to serve contents of an S3 bucket through an API gateway

--- lib/api-gateway-proxy-to-s3-stack.ts
+++ lib/api-gateway-proxy-to-s3-stack.ts
@@ -1,9 +1,39 @@
 import * as cdk from '@aws-cdk/core';
+import {RemovalPolicy, Stack} from '@aws-cdk/core';
+import {HttpIntegration, RestApi} from "@aws-cdk/aws-apigateway";
+import {Bucket} from "@aws-cdk/aws-s3";
+import {BucketDeployment, Source} from "@aws-cdk/aws-s3-deployment";
+import * as path from "path";
 
 export class ApiGatewayProxyToS3Stack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    private api: RestApi;
+    private readonly bucket: Bucket;
+    private bucketDeployment: BucketDeployment;
 
-    // The code that defines your stack goes here
-  }
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        this.bucket = new Bucket(this, 'Bucket', {
+            websiteIndexDocument: "index.html",
+            websiteErrorDocument: 'index.html',
+            publicReadAccess: true,
+
+            removalPolicy: RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
+        this.bucketDeployment = new BucketDeployment(this, 'BucketDeployment', {
+            sources: [
+                Source.asset(path.resolve(__dirname, './../public/'))
+            ],
+            destinationBucket: this.bucket,
+        })
+
+        this.api = new RestApi(this, id + '-RestApi');
+
+        this.api.root
+            .addResource('public')
+            .addProxy({
+                defaultIntegration: new HttpIntegration(this.bucket.bucketWebsiteUrl),
+            });
+    }
 }
